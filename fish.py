6a38dc821bb6e896e958a494a6b23ae92297670e
cekefun <cekefun@gmail.com>
2017-01-08 20:11:24 +0100

    Add files via upload

--- fish.py
+++ fish.py
@@ -1,11 +1,14 @@
 import os
 import numpy as np
 import cv2
+import cPickle as pickle
 import gzip
-from sklearn.neighbors import NearestNeighbors
+import random
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-import pickle
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import log_loss
 
 # Fish Labels. NoF corresponds to No Fish, others are all types of Fish
 LABELS = {
@@ -22,8 +25,8 @@ LABELS = {
 class FishClassifier:
     def __init__(self):
         # Creates dictionary that maps every label to a list of files with that label
-        self.index = 0
-        self.dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Train')
+        self.cluster = MiniBatchKMeans(n_clusters=200)
+        self.dir = 'Desktop/AI/Train'
         self.labelFiles = dict()
         for label in LABELS.keys():
             self.labelFiles[label] = [os.path.join(self.dir, label, ff) \
@@ -31,12 +34,26 @@ class FishClassifier:
                                         os.listdir(os.path.join(self.dir, label)))]
 
 
+    def submission(self):
+        testPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Test')
+        subPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'submission.csv')
+        train = []
+        for label in LABELS.keys():
+            clas = LABELS[label]
+            train += [(fn, clas) for fn in self.labelFiles[label]]
+        test = [os.path.join(testPath, ff) \
+                for ff in filter(lambda fn: fn.endswith(".jpg"), os.listdir(testPath))]
+        preds = self.predict(train, test)
+        with open(subPath, 'wb') as subFile:
+            subFile.write('image,ALB,BET,DOL,LAG,NoF,OTHER,SHARK,YFT\n')
+            for i in range(len(test)):
+                subFile.write(",".join([os.path.basename(test[i])] + [str(prob) for prob in preds[i]]) + "\n")
+
     def crossValidation(self, k):
         scores = []
-        allItems = list(self.labelFiles.values())
-        allItems = sum(allItems,[])
-        indexStart = allItems.index(self.labelFiles["NoF"][0])
-        indexEnd = allItems.index(self.labelFiles["NoF"][-1])
+        testsets = []
+        for list in self.labelFiles.values():
+            random.shuffle(list)
         for i in range(0, k):
             train = []
             test = []
@@ -48,23 +65,24 @@ class FishClassifier:
             # test should be a list of filenames
             # testLabels should be a list of classes in the same order as test
             # Class = 0 if NoFish, Class = 1 if some type of Fish
+            index = 0
+            for key in self.labelFiles.keys():
+                for image in self.labelFiles[key]:
+                    if index == k:
+                        index = 0
+                    if index == i:
+                        test.append(image)
+                        testLabels.append(LABELS[key])
+                        
+                    else:
+                        train.append((image,LABELS[key]))
 
-            for item in range(0,len(allItems)):
-                picture = allItems[item]
-                Class = 1
-                if item >= indexStart and item <= indexEnd:
-                    Class = 0
-
-                if item % k == i:
-                    test.append(picture)
-                    testLabels.append(Class)
-                else:
-                    train.append((picture,Class))
+                    index += 1
             prediction = self.predict(train, test)
-            scores.append(self.score(prediction, testLabels))
+            scores.append(log_loss(testLabels,prediction))
             print (i+1) ," loops complete"
         print scores
-        print "Mean Accuracy:", np.mean([(x1+x2)/2.0 for (x1, x2) in scores])
+        print "Mean Accuracy:", np.mean(scores)
         
         
     def getFeatures(self, train):
@@ -76,46 +94,94 @@ class FishClassifier:
             # Only current feature: count of pixel intensities in grayscale image
             gray = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY) 
             # TODO: Add additional features
-            features.append([np.sum(np.array(gray))])
-            
-            #img_blur = cv2.blur(img_r,(1,1))
-            #edges = cv2.Canny(img_blur,100,200)
-            #features.append([np.sum(np.array(edges))])
 
-            #edges = cv2.Canny(img_blur,200,300)
-            #features.append([np.sum(np.array(edges))])
-
-            #edges = cv2.Canny(img_blur,0,100)
-            #features.append([np.sum(np.array(edges))])
-            
+            img_blur = cv2.blur(img_r,(20,20))
+            edges = cv2.Canny(img_blur,0,100)
+            lapl = cv2.Laplacian(gray,cv2.CV_64F)
+            b,g,r = cv2.split(img_r)
+            surffeatures = self.surf(fn)
+            values = self.histogram(surffeatures)
+            features.append([np.sum(np.array(gray)),np.sum(np.array(edges)),np.sum(np.array(lapl)),np.sum(np.array(r)),np.sum(np.array(g)),np.sum(np.array(b))])
+            features[-1]+= values
         return features
-        
-    def sift(self,train):
-        sifts = {}
-        siftClass = cv2.SIFT()
-        for image in train:
-            img = cv2.imread(image)
-            kp, des = siftClass.detectAndCompute(img,None)
-            sifts[image] = des
-        with gzip.open("test"+str(self.index)+".zip", 'wb') as f:
-            pickle.dump(sifts, f)
 
-        self.index += 1
+    #Makes the surf files of the test sets.   
+    def preprocess(self,test):
+        surfs = {}
+        for image in test:
+            des = self.surf(image)
+            surfs[image] = des
+            with gzip.open(image + ".zip",'wb') as f:
+                pickle.dump(des,f)
+    
+    '''
+    #gets the surf features of a single image. Has to be preprocessed
+    def surf(self,image):
+        features = [];
+        with gzip.open(image + ".zip",'rb') as f:
+            features = pickle.load(f)
+        return features
+    '''
+
+    #gets the surf features of a single image after rescaling and grayscaling    
+    def surf(self, image):
+        surfClass = cv2.SURF()
+        img = cv2.imread(image)
+        img_r = cv2.resize(img,(1280,720))
+        gray = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+        kp,des = surfClass.detectAndCompute(gray,None)
+        return des        
+    
+    #cluster for small amount of data    
+    def make_cluster(self,data):
+        all_surfs = self.surf(data[0][0])
+        for image in data:
+            if(image == data[0][0]):
+                continue
+            li = self.surf(image[0])
+            toAdd = np.array(li)
+            all_surfs = np.vstack((all_surfs,toAdd))
+        self.cluster.fit(all_surfs)
+    '''
+    #cluster for with predone surf features    
+    def make_cluster(self,data):
+        self.cluster = MiniBatchKMeans(self.cluster.get_params()['n_clusters'])
+        for i in data:
+            i = self.surf(data[0])
+            self.cluster.partial_fit(features)
+    '''      
+    def histogram(self,array):
+        labels = self.cluster.predict(array)
+        result = {}
+        for i in range(0,self.cluster.get_params()['n_clusters']):
+            result[i] = 0
+        for label in labels:
+            result[label]+=1
+        for i in result.keys():
+            result[i] = float(result[i])/(float(array.size)/float(128))*float(10000) #because i find the values too small
+        arrayResult = []
+        for i in result.values():
+            arrayResult.append(i)
+        return arrayResult
             
-            
+        
+        
     def predict(self, train, test):
-        self.sift(test)
-        noFishTrain = [f for f, l in filter(lambda (fn, lab) : lab == 0, train)]
-        fishTrain = [f for f, l in filter(lambda (fn, lab) : lab == 1, train)]
-        fishF = self.getFeatures(fishTrain)
-        noFishF = self.getFeatures(noFishTrain)
+        self.make_cluster(train)
+        print "Done with surfing and clustering"
+        Train = [f for (f,i) in train]
+        #noFishTrain = [f for f, l in filter(lambda (fn, lab) : lab == 0, train)]
+        #fishTrain = [f for f, l in filter(lambda (fn, lab) : lab == 1, train)]
+        #fishF = self.getFeatures(fishTrain)
+        #noFishF = self.getFeatures(noFishTrain)
         testF = self.getFeatures(test)
-        X = fishF + noFishF
-        y = [1 for x in fishF] + [0 for x in noFishF]
+        X = self.getFeatures(Train)
+        y = [i for (f,i) in train]
         # TODO: Fit a classifier
-        classifier = RandomForestClassifier()
+        classifier = RandomForestClassifier(100)
         classifier.fit(X,y)
-        return classifier.predict(testF)
+        prediction = classifier.predict_proba(testF)
+        return prediction
         
         
     def score(self, prediction, testLabels):
@@ -134,6 +200,63 @@ class FishClassifier:
                 oneCount += 1
         return (zeroCorr/float(zeroCount), oneCorr/float(oneCount))
 
+
+    def scoreMulti(self, prediction, testLabels):
+        counts = dict()
+        corrects = dict()
+        for lab in LABELS.keys():
+            c = LABELS[lab]
+            counts[c] = 0
+            corrects[c] = 0
+    
+        for i in range(len(testLabels)):
+            l = testLabels[i]
+            p = prediction[i]
+            counts[l] += 1
+            if l == p:
+                corrects[l] += 1
+        return tuple([corrects[c]/float(counts[c]) for c in counts.keys()])
+
+
+def match(pattern, tup):
+    for (ix, val) in pattern:
+        if tup[ix] != val: return False
+    return True
+
+def getAttrFromItemset(itemset, attrIx):
+    for (ix, val) in itemset:
+        if ix == attrIx: return val
+    return -1
+
+# Expects a matrix where every row is a transaction
+# Returns a list of (itemset, support) pairs
+# Itemset is a list of (attrIndex, value) pairs
+def eclat(transactionTable, minsup):
+    data = {}
+    for rowIx in range(len(transactionTable)):
+        row = transactionTable[rowIx]
+        for item in enumerate(row):
+            if not item in data:
+                data[item] = set([rowIx])
+            else:
+                data[item].add(rowIx)
+    return eclatImpl([], data.items(), minsup)
+
+# http://adrem.ua.ac.be/~goethals/software/files/eclat.py
+def eclatImpl(prefix, items, minsup):
+    output = []
+    while items:
+        i,itids = items.pop()
+        isupp = len(itids)
+        if isupp >= minsup:
+            output.append((sorted(prefix+[i]), isupp))
+            suffix = [] 
+            for j, ojtids in items:
+                jtids = itids & ojtids
+                if len(jtids) >= minsup:
+                    suffix.append((j,jtids))
+            output += eclatImpl(prefix+[i], sorted(suffix, key=lambda item: len(item[1]), reverse=True), minsup)
+    return output
 
 if __name__ == '__main__':
     fc = FishClassifier()
